d7c2598a861fd6a340d9b9d5388e5a1f2220cbaf
Luyuan Xing <lxing.nyc@gmail.com>
2016-07-10 15:13:35 -0400

    rake db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710190027) do
+ActiveRecord::Schema.define(version: 20160710191007) do
 
   create_table "down_votes", force: :cascade do |t|
     t.integer  "topic_id"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20160710190027) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "topics", ["user_id"], name: "index_topics_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
